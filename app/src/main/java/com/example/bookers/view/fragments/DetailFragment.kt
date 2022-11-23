312b99c7819c79e25d6808e56fd7b76c065284ca
alan.marcos.7e5 <alan.marcos.7e5@itb.cat>
2022-11-23 20:05:26 +0100

    Agregada imagen en detail, falta ajustar tamaños

--- app/src/main/java/com/example/bookers/view/fragments/DetailFragment.kt
+++ app/src/main/java/com/example/bookers/view/fragments/DetailFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.bookers.databinding.FragmentDetailBinding
 import com.example.bookers.viewModel.BookersViewModel
 
@@ -29,6 +31,12 @@ class DetailFragment : Fragment() {
         //binding.bookImageIv.setImageResource()
         binding.bookTitleTv.text = model.selectedBook.value!!.title
         binding.bookDescriptionTv.text = model.selectedBook.value!!.description
+        Glide.with(this)
+            .load(model.selectedBook.value!!.image)
+            .diskCacheStrategy(DiskCacheStrategy.ALL) //save in cache to avoid unneeded resources consume
+            .centerCrop()
+            .circleCrop()
+            .into(binding.bookImageIv) //put the image in te image view
     }
 
 }
